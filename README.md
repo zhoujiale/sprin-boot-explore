7175002e334e07e3580c2ff4509e1de8df5d5a78
zhoujiale <1330690025@qq.com>
2020-10-02 16:39:24 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,2 @@
+# spring_boot_explore
+spring boot探索
---
+++ spring_boot_shiro/src/main/java/com/zjl/spring_boot_shiro/SpringBootShiroApplication.java
@@ -0,0 +1,13 @@
+package com.zjl.spring_boot_shiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootShiroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootShiroApplication.class, args);
+    }
+
+}
