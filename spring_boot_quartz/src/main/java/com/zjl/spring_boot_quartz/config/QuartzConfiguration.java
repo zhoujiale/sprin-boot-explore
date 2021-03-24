8019e1e5a862dd6d68709d5b6c073fc1342701de
13588330822 <13588330822@163.com>
2021-03-24 10:29:04 +0800

    quartz 配置

--- spring_boot_quartz/src/main/java/com/zjl/spring_boot_quartz/config/QuartzConfiguration.java
+++ spring_boot_quartz/src/main/java/com/zjl/spring_boot_quartz/config/QuartzConfiguration.java
@@ -1,6 +1,11 @@
 package com.zjl.spring_boot_quartz.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+
+import javax.sql.DataSource;
+import java.util.Properties;
 
 /**
  * @author zhou
@@ -12,5 +17,32 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class QuartzConfiguration {
 
+    private static final String SCHEDULER_NAME = "Self_Scheduler";
 
+    private static final String SCHEDULER_CONTEXT_KEY = "applicationContextKey";
+
+    @Bean
+    public SchedulerFactoryBean schedulerFactoryBean(DataSource dataSource,Properties quartzProperties){
+        SchedulerFactoryBean schedulerFactoryBean = new SchedulerFactoryBean();
+        schedulerFactoryBean.setDataSource(dataSource);
+        schedulerFactoryBean.setSchedulerName(SCHEDULER_NAME);
+        schedulerFactoryBean.setQuartzProperties(quartzProperties);
+        schedulerFactoryBean.setApplicationContextSchedulerContextKey(SCHEDULER_CONTEXT_KEY);
+        return schedulerFactoryBean;
+    }
+
+    @Bean
+    public Properties quartzProperties(){
+        Properties properties = new Properties();
+        //定时器实例名称
+        properties.put("org.quartz.scheduler.instanceName","SelfQuartzScheduler");
+        properties.put("org.quartz.scheduler.instanceId","AUTO");
+        //线程池
+        properties.put("org.quartz.threadPool.class","org.quartz.simpl.SimpleThreadPool");
+        properties.put("org.quartz.threadPool.threadCount","20");
+        properties.put("org.quartz.threadPool.threadPriority","5");
+        //jobStore
+
+        return properties;
+    }
 }
