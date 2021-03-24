9d25565aa8390d7c9a361320a1812ee8c0b046fe
13588330822 <13588330822@163.com>
2021-03-24 10:28:31 +0800

    netty 初始化

---
+++ spring_boot_netty/src/main/java/com/zjl/spring_boot_netty/SpringBootNettyApplication.java
@@ -0,0 +1,13 @@
+package com.zjl.spring_boot_netty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNettyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootNettyApplication.class, args);
+    }
+
+}
