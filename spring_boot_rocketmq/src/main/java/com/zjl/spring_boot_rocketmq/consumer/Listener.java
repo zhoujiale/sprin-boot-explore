d86e936372e24a59745ad77f59751dec78c8a7c8
zhoujiale <shangchuang@rokid.com>
2023-05-15 16:58:22 +0800

    rocketmq

--- spring_boot_rocketmq/src/main/java/com/zjl/spring_boot_rocketmq/consumer/Listener.java
+++ spring_boot_rocketmq/src/main/java/com/zjl/spring_boot_rocketmq/consumer/Listener.java
@@ -1,9 +1,7 @@
 package com.zjl.spring_boot_rocketmq.consumer;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
-import org.springframework.stereotype.Component;
 
 /**
  * @author zhou
@@ -12,8 +10,8 @@ import org.springframework.stereotype.Component;
  * @date 2022/7/7 17:37
  */
 @Slf4j
-@Component
-@RocketMQMessageListener(topic = "spring-topic",selectorExpression = "one || two",consumerGroup = "self-consumer-group")
+//@Component
+//@RocketMQMessageListener(topic = "spring-topic",selectorExpression = "one || two",consumerGroup = "self-consumer-group")
 public class Listener implements RocketMQListener<String> {
     @Override
     public void onMessage(String s) {
