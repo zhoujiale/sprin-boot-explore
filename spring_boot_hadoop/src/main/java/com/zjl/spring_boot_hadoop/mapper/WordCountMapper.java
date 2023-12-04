b60dc3b1729502f085b24a0bb4598a22885b0604
zhoujiale <1330690025@qq.com>
2023-12-04 13:23:29 +0800

    spring hadoop

---
+++ spring_boot_hadoop/src/main/java/com/zjl/spring_boot_hadoop/mapper/WordCountMapper.java
@@ -0,0 +1,30 @@
+package com.zjl.spring_boot_hadoop.mapper;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+/**
+ * @classname: WordCountMapper
+ * @author: zhou
+ * @description:
+ * @date: 2023/10/20 14:46
+ */
+public class WordCountMapper extends Mapper<LongWritable, Text,Text, IntWritable> {
+
+    Text k = new Text();
+    IntWritable v = new IntWritable(1);
+
+    @Override
+    protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, IntWritable>.Context context) throws IOException, InterruptedException {
+        String line = value.toString();
+        String[] words = line.split(" ");
+        for (String word : words) {
+            k.set(word);
+            context.write(k,v);
+        }
+    }
+}
---
+++ spring_boot_hadoop/src/test/java/com/zjl/spring_boot_hadoop/SpringBootHadoopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjl.spring_boot_hadoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHadoopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
